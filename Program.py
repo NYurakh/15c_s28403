dc168e09b2555f0b03e8195aebd6f39049a637ee
s28403 <s28403@pjwstk.edu.pl>
2024-03-19 18:22:57 +0100

    Made task 2 (function to generate list of squares)

--- Program.py
+++ Program.py
@@ -4,3 +4,13 @@ print("# Task 1")
 squares = [x**2 for x in range(1, 11)]
 
 print(squares)
+
+
+
+# Task 2
+print("# Task 2")
+
+def e_squares(start, end):
+    return [x**2 for x in range(start, end+1)]
+
+print(e_squares(1, 10))
